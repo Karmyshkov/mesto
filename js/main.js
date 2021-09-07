524d72c39e12375a524b27fb61bf4bd074a253ad
Evgeniy Karmyskov <67991583+Karmyshkov@users.noreply.github.com>
2021-09-07 22:50:13 +0300

    feat: add logic for work with popap

---
+++ js/main.js
@@ -0,0 +1,45 @@
+let popup = document.querySelector('.popup');
+let formElement = document.querySelector('.popup__container');
+let nameInput = document.querySelector('input[name="userName"]');
+let jobInput = document.querySelector('input[name="userJob"]');
+
+let btnEdit = document.querySelector('.profile__edit');
+let btnClose = formElement.querySelector('.popup__close');
+let btnSave = formElement.querySelector('.popup__btn');
+
+let currentValueName = document.querySelector('.profile__name');
+let currentValueJob = document.querySelector('.profile__description');
+
+btnEdit.addEventListener('click', function(evt) {
+  evt.preventDefault();
+  popup.classList.add('popup_opened');
+});
+
+btnClose.addEventListener('click', function(evt) {
+  evt.preventDefault();
+  popup.classList.remove('popup_opened');
+});
+
+/** 
+ * Добавлеяет текст в input
+ * param {object} name, объект содержащий имя пользователя
+ * param {object} job, объект содержаший информацию об работе
+*/
+
+function renderData (name, job) {
+  nameInput.value  = name.innerHTML;
+  jobInput.value = job.innerHTML;
+}
+
+renderData(currentValueName, currentValueJob);
+
+function formSubmitHandler (evt) {
+    evt.preventDefault(); 
+    
+    currentValueName.textContent = nameInput.value;
+    currentValueJob.textContent = jobInput.value;
+
+    popup.classList.remove('popup_opened');
+}
+
+formElement.addEventListener('submit', formSubmitHandler); 
