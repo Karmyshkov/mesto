3e00d635737a123e7283a9d563f853ab904775f8
Evgeniy Karmyskov <67991583+Karmyshkov@users.noreply.github.com>
2021-10-06 16:05:22 +0300

    feat: add open and close function

--- js/main.js
+++ js/main.js
@@ -1,20 +1,27 @@
-const popup = document.querySelector('.popup');
+const popupEditProfile = document.querySelector('.popup_type_edit-profile');
+const popupAddCard = document.querySelector('.popup_type_add-card');
 const formElement = document.querySelector('.popup__container');
 const nameInput = document.getElementById('userName');
 const jobInput = document.getElementById('userJob');
 const btnEdit = document.querySelector('.profile__edit');
-const btnClose = formElement.querySelector('.popup__close');
+const btnAddCard = document.querySelector('.profile__btn');
+const btnCloseEditForm = popupEditProfile.querySelector('.popup__close');
+const btnCloseAddCardForm = popupAddCard.querySelector('.popup__close');
 const currentValueName = document.querySelector('.profile__name');
 const currentValueJob = document.querySelector('.profile__description');
 
-const openForm = () => {
-  popup.classList.add('popup_opened');
+const addInfoForm = () => {
   nameInput.value  = currentValueName.textContent;
   jobInput.value = currentValueJob.textContent;
 }
 
-const closePopap = () => {
-  popup.classList.remove('popup_opened');
+const openForm = (element, fnc = null) => {
+  element.classList.add('popup_opened');
+  if (typeof fnc !== "object") fnc();
+}
+
+const closePopap = element => {
+  element.classList.remove('popup_opened');
 }
 
 const formSubmitHandler = evt => {
@@ -23,9 +30,12 @@ const formSubmitHandler = evt => {
     currentValueName.textContent = nameInput.value;
     currentValueJob.textContent = jobInput.value;
 
-    closePopap();
+    closePopap(popupEditProfile);
 }
 
-btnEdit.addEventListener('click', openForm);
-btnClose.addEventListener('click', closePopap);
+btnEdit.addEventListener('click', () => openForm(popupEditProfile, addInfoForm));
+btnCloseEditForm.addEventListener('click', () => closePopap(popupEditProfile));
+btnAddCard.addEventListener('click', () => openForm(popupAddCard));
+btnCloseAddCardForm.addEventListener('click', () => closePopap(popupAddCard));
+
 formElement.addEventListener('submit', formSubmitHandler); 
