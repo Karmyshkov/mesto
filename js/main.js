9b2b9a0b3d62f7b834787d3d0bf8be0bd68d4d64
Evgeniy Karmyskov <67991583+Karmyshkov@users.noreply.github.com>
2021-10-06 19:25:02 +0300

    feat: add obj with data card

--- js/main.js
+++ js/main.js
@@ -1,3 +1,6 @@
+const cardTemplate = document.querySelector('.card-template');
+const cardList = document.querySelector('.places__cards');
+
 const popupEditProfile = document.querySelector('.popup_type_edit-profile');
 const popupAddCard = document.querySelector('.popup_type_add-card');
 const formElement = document.querySelector('.popup__container');
@@ -9,6 +12,44 @@ const btnCloseEditForm = popupEditProfile.querySelector('.popup__close');
 const btnCloseAddCardForm = popupAddCard.querySelector('.popup__close');
 const currentValueName = document.querySelector('.profile__name');
 const currentValueJob = document.querySelector('.profile__description');
+
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+
+const renderCard = obj => {
+  const newCard = cardTemplate.content.cloneNode(true);
+
+  newCard.querySelector('.card__img').src = obj.link;
+  newCard.querySelector('.card__title').textContent = obj.name;
+
+  cardList.append(newCard);
+}
+
+initialCards.map(renderCard);
 
 const addInfoForm = () => {
   nameInput.value  = currentValueName.textContent;
