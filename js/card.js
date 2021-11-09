50b0ecb2f568ed68b98db4929257e7bd092f2864
karmyskov <karmyskove@gmail.com>
2021-11-09 17:35:08 +0300

    refactor: change open/close popup function

--- js/card.js
+++ js/card.js
@@ -1,39 +1,61 @@
-import {cardList} from '../js/constants.js';
+import * as constants from '../js/constants.js';
 
 export class Card {
-    constructor (data, template) {
-        this.img = data.link;
-        this.title = data.name;
-        this.isLike = false;
-        this.template = template;
-    }
+	constructor (data, template) {
+		this.img = data.link;
+		this.title = data.name;
+		this.isLike = false;
+		this.template = template;
+	}
 
-    _deleteCard (elem) {
-        const btnDelete = elem.querySelector('.card__btn_type_delete');
+	_setAddEventListener () {
+		constants.btnEdit.addEventListener('click', () => {
+			this._openPopup(constants.popupEditProfile);
+			// addInfoForm();
+		});
+		constants.btnAddCard.addEventListener('click', () => this._openPopup(constants.popupAddCard));
+    constants.btnCloseFormProfile.addEventListener('click', this._closePopup);
+    constants.btnCloseFormAddCard.addEventListener('click', this._closePopup);
+    constants.btnCloseFormMore.addEventListener('click', this._closePopup);
+	}
 
-        btnDelete.addEventListener('click', function (evt) {
-          const currentElement = evt.target;
-          currentElement.parentElement.remove();
-        });
-    }
+	_openPopup (elem) {
+		elem.classList.add('popup_opened');
+		//document.addEventListener('keydown', closeByEscape);
+	}
 
-    createCard () {
-        const newCard = this.template.content.cloneNode(true);
-        const cardTitle = newCard.querySelector('.card__title');
-        const cardImg = newCard.querySelector('.card__img');
+	_closePopup () {
+		const activePopup = document.querySelector('.popup_opened');
+		if (activePopup) activePopup.classList.remove('popup_opened');
+		// document.removeEventListener('keydown', closeByEscape); 
+	}
 
-        cardTitle.textContent = this.title;
-        cardImg.src = this.img;
-        cardImg.alt = `Фото из ${this.title}`;
+	_deleteCard (elem) {
+		const btnDelete = elem.querySelector('.card__btn_type_delete');
 
-        this._deleteCard(newCard);
+		btnDelete.addEventListener('click', function (evt) {
+		  const currentElement = evt.target;
+		  currentElement.parentElement.remove();
+		});
+	}
 
-        return newCard;
-    }
+	_createCard () {
+		const newCard = this.template.content.cloneNode(true);
+		const cardTitle = newCard.querySelector('.card__title');
+		const cardImg = newCard.querySelector('.card__img');
 
-    renderCard () {
-        const cards = this.createCard();
-        cardList.prepend(cards)
-    }
+		cardTitle.textContent = this.title;
+		cardImg.src = this.img;
+		cardImg.alt = `Фото из ${this.title}`;
+		
+		this._setAddEventListener();
+		this._deleteCard(newCard);
 
+		return newCard;
+	}
+
+	renderCard () {
+		const cards = this._createCard();
+		constants.cardList.prepend(cards)
+	}
 }
